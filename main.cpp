ab00a6a8d0372cb1efc42ec1578359d93b05a5ff
창환 <63768572+ckdghks@users.noreply.github.com>
2021-10-20 06:10:55 +0000

    찐막

--- main.cpp
+++ main.cpp
@@ -1,41 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class Rect; 
-
-class RectManager {
-public:
-  bool equals(Rect r, Rect s);
-  void copy(Rect& dest, Rect& src);
-};
-
-class Rect {
-  int width, height;
-public:
-  Rect(int width, int height) { this->width = width; this->height = height;}
-  friend RectManager;
-  void getdata(){ cout << width << " " << height << endl;}
-};
-
-bool RectManager::equals(Rect r, Rect s) {
-  if(r.width == s.width && r.height == s.height) return true;
-  else return false;  
+int big(int a, int b) {
+  if(a>b) return a;
+  else return b;
 }
-
-void RectManager::copy(Rect& dest, Rect& src) {
-  dest.width = src.width;
-  dest.height = src.height;
+int big(int a[], int size) {
+  int res = a[0];
+  for(int i=1; i<size; i++) {
+    if(res < a[i]) res = a[i];
+  }
+  return res;
 }
 
 int main() {
-  Rect a(3, 4), b(5, 6);
-  RectManager man;
-  if(man.equals(a, b)) cout << "equal" << endl;
-  else cout << "not equal" << endl;
-  a.getdata(); b.getdata();
-
-  man.copy(b, a); // a를 b에 복사한다.
-  if(man.equals(a, b)) cout << "equal" << endl;
-  else cout << "not equal" << endl;
-  a.getdata(); b.getdata();
+  int array[5] = {1, 9, -2, 8, 6};
+  cout << big(2, 3) << endl;
+  cout << big(array, 5) << endl;
 }
